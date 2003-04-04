d071310df67816b317949305fab3ff99e28cab80
Jeff Epler <jepler@unpythonic.net>
2003-04-04 16:01:38 +0000

    distutils fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup
 
+name="Novelwriting"
+version="0.2"
+DOCDIR="/usr/share/doc/%s-%s" % (name, version)
 setup(
-    name="Novelwriting",
-    version="0.2",
+    name=name,
+    version=version,
     description="Rule-based text generation",
     author="Jeff Epler",
     author_email="jepler@unpythonic.net",
     url="http://unpythonic.net/jeff/novelwriting/",
     packages=['novelwriting'],
-    scripts=['scripts/novelwriting']
+    scripts=['scripts/novelwriting'],
+    data_files=[(DOCDIR + "/examples",
+                    [ "examples/buy.nw", "examples/nw.nw",
+                      "examples/simple.nw", "examples/test.nw"]),
+                (DOCDIR, ["README.html"])],
 )
